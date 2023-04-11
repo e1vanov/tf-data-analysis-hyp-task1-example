ed2e213c31411db3e8442afe75c463c70edbb4f4
e1vanov <99652767+e1vanov@users.noreply.github.com>
2023-04-11 23:47:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 464814509 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    stat, pval = proportions_ztest(count, nobs, alternative='two-sided')
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return pval > 0.92
