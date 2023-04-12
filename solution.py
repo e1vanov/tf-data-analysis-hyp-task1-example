04308701674be2f72600195f01b00772ecaef46a
e1vanov <99652767+e1vanov@users.noreply.github.com>
2023-04-12 22:33:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def solution(x_success: int,
              y_cnt: int) -> bool:
     count = np.array([x_success, y_success])
     nobs = np.array([x_cnt, y_cnt])
-    stat, pval = proportions_ztest(count, nobs, alternative='two-sided')
+    stat, pval = proportions_ztest(count, nobs)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
